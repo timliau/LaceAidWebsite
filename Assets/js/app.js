2ceeed49fddf244269d67f36c1ec0bf339238579
setiawansandi <81544477+setiawansandi@users.noreply.github.com>
2022-02-11 10:21:22 +0800

    added sign up

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -6,14 +6,18 @@ var hamburger_menu;
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-    alert("user is signed in");
+    console.log("user is signed in");
 
     var user = firebase.auth().currentUser;
 
     if(user != null){
+      console.log("User is not null")
+      // document.getElementById("hidelogin").style.display = "none";
 
-      alert("User is not null")
+      var change = document.getElementById('output');
+      var tochange = '<li class="dropdown"><img src="./img/img_avatar.png" style="border-radius: 50%; height: 30px; margin-left: 4.5rem; width: 30px;" alt="Avatar"></a><ul><li><a href="#">Profile</a></li><li><a href="#">Measurement</a></li><li><a onclick="logout()" style="cursor: pointer;">Logout</a></li></ul></li>';
 
+      change.innerHTML = tochange;
     }
 
   } else {
@@ -21,6 +25,17 @@ firebase.auth().onAuthStateChanged(function(user) {
     console.log("no user is signed in")
   }
 });
+
+function logout(){
+  firebase.auth().signOut();
+  console.log("logout");
+  // document.getElementById("hidelogin").style.display = "block";
+  location.reload();
+  // var change = document.getElementById('output');
+  // var tochange = '<a href="./signup.html" class="btn" id="hidelogin">Sign up</a>';
+
+  // change.innerHTML = tochange;
+}
 
 function declare() {
   toggle_btn = document.querySelector(".toggle-btn");
--- Assets/js/signup.js
+++ Assets/js/signup.js
@@ -46,3 +46,22 @@ function login(){
   });
 
 }
+
+function signup(){
+
+  var userEmail = document.getElementById("pemail").value;
+  var userPass = document.getElementById("ppassword").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+    window.alert("Error : " + errorMessage);
+  });
+}
